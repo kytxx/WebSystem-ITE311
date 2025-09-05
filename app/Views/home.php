9769d4ed71938dbc5932f0a28736144d5393c4f9
CHAN-07 <158530419+CHAN-07@users.noreply.github.com>
2025-09-05 14:19:15 +0800

    Initial commit - CodeIgniter setup

--- app/Views/home.php
+++ app/Views/home.php
@@ -2,7 +2,7 @@
 
 <?= $this->section('content') ?>
     <div class="text-center mt-5">
-        <h1>Welcome to CodeIgniter!</h1>
+        <h1>Welcome to CodeIgniter.!</h1>
         <p class="lead">This is your Bootstrap Integrated home page.</p>
     </div>
 <?= $this->endSection() ?>
